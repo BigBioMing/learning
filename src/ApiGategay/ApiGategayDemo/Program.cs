8e6e10d54bf259f6b2e348d50f80fbd03294cb03
BigBioMing <wocxming@sina.com>
2023-02-09 01:17:28 +0800

    网关

--- src/ApiGategay/ApiGategayDemo/Program.cs
+++ src/ApiGategay/ApiGategayDemo/Program.cs
@@ -6,11 +6,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
-builder.Services.AddOcelot();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddOcelot();
 
 var app = builder.Build();
 
--- src/ApiGategay/Mobile.Aggregate.Api/Program.cs
+++ src/ApiGategay/Mobile.Aggregate.Api/Program.cs
@@ -1,3 +1,5 @@
+using Mobile.Aggregate.Api;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpClient<ITypeOrderHttpClient, TypeOrderHttpClient>()
+    .SetHandlerLifetime(TimeSpan.FromMinutes(3))
+    .ConfigureHttpClient(config =>
+    {
+        config.BaseAddress = new Uri("https://localhost:7013");
+    });
 
 var app = builder.Build();
 
