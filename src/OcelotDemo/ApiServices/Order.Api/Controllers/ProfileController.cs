6f5a4c28fec033c00446816c96d5c5dee3be1bc2
BigBioMing <wocxming@sina.com>
2023-10-23 19:09:02 +0800

    12

---
+++ src/OcelotDemo/ApiServices/Order.Api/Controllers/ProfileController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Order.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfileController : ControllerBase
+    {
+        [HttpGet]
+        [Route("info")]
+        public IActionResult Info()
+        {
+            return Ok("我是order.api");
+        }
+    }
+}
---
+++ src/OcelotDemo/OcelotDemo01/ApiGategay_OcelotDemo01/Controllers/ProfileController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiGategay_OcelotDemo01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfileController : ControllerBase
+    {
+        [HttpGet]
+        [Route("info")]
+        public IActionResult Info()
+        {
+            return Ok("我是ApiGategay_OcelotDemo01");
+        }
+    }
+}
